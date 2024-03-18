4f2dbd658b1d2d4fe56439691bbfa8fc3aa88d7b
Benjamin Gwynn <git@benjamingwynn.com>
2024-03-18 15:54:47 +0000

    Keep HTML tags in markdown output

--- print.mjs
+++ print.mjs
@@ -1,6 +1,11 @@
-import { markdown, parse } from "markdown";
+#!/usr/bin/env -S node
+import md from "markdown-it";
 import fsp from "fs/promises";
 
 const file = await fsp.readFile("./final.md");
-const out = markdown.toHTML(file.toString("utf-8"));
+const markdown = md({
+  // enable HTML tags like <small
+  html: true,
+});
+const out = markdown.render(file.toString("utf-8"));
 await fsp.writeFile("final.html", out);
