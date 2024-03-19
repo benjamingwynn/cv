380732360621945cf4da7c82dbc9eabb0e89ff4c
Benjamin Gwynn <git@benjamingwynn.com>
2024-03-19 17:02:15 +0000

    cleanup http dev server

--- build/watch.mjs
+++ build/watch.mjs
@@ -13,17 +13,17 @@ import {status} from "./common.mjs"
 
 async function makeDocument() {
 	let doc = await renderDocument()
-	doc = `<html><head><title>Document preview</title></head><body>` + doc + `</body>`
+	doc = `<!doctype html><html><head><title>Document preview</title></head><body>` + doc + `</body></html>`
 	// add the hot reload client-side code
 	doc += `\n<script>(new EventSource("$")).onmessage=function(){location.reload(!0)}</script>`
 	// add styling so it always looks like a pdf when in dev mode
 	doc += `<style>
 	@media only screen {
 		html {
-			background: #3b4750;
+			background-color: #aaa;
 		}
 		body {
-			background: white;
+			background-color: white;
 			box-shadow: black 0 0 1em;
 			margin: 0.25in auto;
 			padding: 0.5in;
@@ -37,30 +37,38 @@ async function makeDocument() {
 
 let document = await makeDocument()
 
-/** sends an event to all clients on teh data stream*/
+/** sends an event to the open data stream `req` */
 function sendEvent(res) {
 	res.write("event: message\n")
 	res.write(`data: \n\n`)
 }
-const connections = new Set()
+const eventStreamConnections = new Set()
 const server = http.createServer((req, res) => {
-	// use the /$ path as a special hook for auto-reload
-	if (req.url === "/$") {
-		res.writeHead(200, {
-			"Content-Type": "text/event-stream",
-			"Cache-Control": "no-cache",
-			Connection: "keep-alive",
-		})
-		connections.add(res)
-		req.on("close", () => {
-			connections.delete(res)
-		})
-		return
+	switch (req.url) {
+		case "/$": {
+			// use the /$ path as a special hook for auto-reload
+			res.writeHead(200, {
+				"Content-Type": "text/event-stream",
+				"Cache-Control": "no-cache",
+				Connection: "keep-alive",
+			})
+			eventStreamConnections.add(res)
+			req.on("close", () => {
+				eventStreamConnections.delete(res)
+			})
+			return
+		}
+		case "/favicon.ico": {
+			res.writeHead(404)
+			res.end("Favicon not found.")
+			return
+		}
+		default: {
+			// serve document
+			res.writeHead(200, {"Content-Type": "text/html"})
+			res.end(document)
+		}
 	}
-
-	// serve document
-	res.writeHead(200, {"Content-Type": "text/html"})
-	res.end(document)
 })
 
 status("Starting HTTP server")
@@ -69,22 +77,22 @@ await listen(8080)
 status(null)
 console.log("listening on", server.address())
 
-let promise
+let makeDocumentPromise
 fs.watch("document.md", () => {
 	console.log("* document.md changed")
-	if (promise) return
+	if (makeDocumentPromise) return
 	console.log("* rebuilding")
-	promise = makeDocument()
+	makeDocumentPromise = makeDocument()
 		.then((newDocument) => {
 			document = newDocument
 			console.log("* document.md rebuilt")
 			// reload everything
-			connections.forEach(sendEvent)
+			eventStreamConnections.forEach(sendEvent)
 		})
 		.catch((err) => {
 			console.error("[render document error]", err)
 		})
 		.finally(() => {
-			promise = null
+			makeDocumentPromise = null
 		})
 })
